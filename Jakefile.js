92a3c4453c6272e1cd9ae711e292a15dad3e3978
David Bedford <beds2004@hotmail.com>
2016-01-29 23:31:54 +0000

    Using jake for our build

---
+++ Jakefile.js
@@ -0,0 +1,12 @@
+/**
+ * Created by djbedford on 29/01/16.
+ */
+
+(function () {
+    'use strict';
+
+    desc('Default build');
+    task('default', function() {
+        console.log('\n\nBUILD OK');
+    });
+})();
