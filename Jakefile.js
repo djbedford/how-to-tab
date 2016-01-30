4906800cf727228696e9407d6d68c05b5ea9b294
David Bedford <beds2004@hotmail.com>
2016-01-30 17:03:23 +0000

    Added jshint to Jakefile

--- Jakefile.js
+++ Jakefile.js
@@ -5,22 +5,29 @@
 (function () {
     'use strict';
 
-    let semver = require('semver');
+    var semver = require('semver');
 
     desc('Default build');
-    task('default', ['version'], function() {
+    task('default', ['version', 'lint'], function () {
         console.log('\n\nBUILD OK');
     });
 
     desc('Check Node version');
-    task('version', function() {
+    task('version', function () {
         console.log('Checking Node version: .');
-        let packageJson = require('./package.json');
-        let expectedVersion = packageJson.engines.node;
-        let actualVersion = process.version;
+        var packageJson = require('./package.json');
+        var expectedVersion = packageJson.engines.node;
+        var actualVersion = process.version;
 
         if (semver.neq(expectedVersion, actualVersion)) {
             fail('Incorrect Node version: expected ' + expectedVersion + ' but was ' + actualVersion);
         }
     });
+
+    desc('Lint JavaScript code');
+    task('lint', function () {
+        console.log('Linting JavaScript: .');
+
+        jake.exec('node node_modules/jshint/bin/jshint Jakefile.js', { interactive: true }, complete);
+    }, { async: true });
 })();
