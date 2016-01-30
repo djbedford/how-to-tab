195a1bf2022a72cdab605e70e6d143457a965c41
David Bedford <beds2004@hotmail.com>
2016-01-30 12:48:03 +0000

    Build script checks Node version

--- Jakefile.js
+++ Jakefile.js
@@ -5,8 +5,21 @@
 (function () {
     'use strict';
 
+    var EXPECTED_NODE_VERSION = 'v5.1.0';
+
     desc('Default build');
-    task('default', function() {
+    task('default', ['version'], function() {
         console.log('\n\nBUILD OK');
+    });
+
+    desc('Check Node version');
+    task('version', function() {
+        console.log('Checking Node version: .');
+
+        let actualVersion = process.version;
+
+        if (actualVersion !== EXPECTED_NODE_VERSION) {
+            fail('Incorrect Node version: expected ' + EXPECTED_NODE_VERSION + ' but was ' + actualVersion);
+        }
     });
 })();
