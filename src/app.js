a2e7fef6fcf6368f1226067ab662f7367d5e7dd2
David Bedford <beds2004@hotmail.com>
2016-02-07 22:20:46 +0000

    Created simple demonstration app

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 /**
  * Created by David on 07/02/2016.
  */
+
+(function() {
+    'use strict';
+
+    var addition = require('./addition.js');
+
+    console.log('Hello!');
+    console.log('42 + 13 = ' + addition.add(42, 13));
+})();
