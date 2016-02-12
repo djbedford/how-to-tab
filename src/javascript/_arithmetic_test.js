3f62612591497313de55e95a8e974f28626c704a
David Bedford <beds2004@hotmail.com>
2016-02-12 23:19:36 +0000

    Set up bare-bones tab example

--- src/javascript/_arithmetic_test.js
+++ src/javascript/_arithmetic_test.js
@@ -5,22 +5,11 @@
 (function () {
     'use strict';
 
-    var arithmetic = require('./arithmetic.js');
     var assert = require('./assert.js');
 
-    describe('Addition', function () {
-        it('adds positive numbers', function () {
-            assert.equal(arithmetic.add(3, 4), 7);
-        });
+    describe('Something', function () {
+        it('something', function () {
 
-        it('uses IEEE 754 floating point', function () {
-            assert.equal(arithmetic.add(0.1, 0.2), 0.30000000000000004);
-        });
-    });
-
-    describe('Subtraction', function () {
-        it('subtracts positive numbers', function () {
-            assert.equal(arithmetic.subtract(10, 3), 7);
         });
     });
 })();
--- src/javascript/app.js
+++ src/javascript/app.js
@@ -5,8 +5,5 @@
 (function() {
     'use strict';
 
-    var arithmetic = require('./arithmetic.js');
 
-    console.log('Hello!');
-    console.log('42 + 13 = ' + arithmetic.add(42, 13));
 })();
